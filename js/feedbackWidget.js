c4044e7b37d5819b243f1b28e69cf3a2a98fbafc
Roelof Roos <github@roelof.io>
2020-02-10 15:16:01 +0100

    history

--- js/feedbackWidget.js
+++ js/feedbackWidget.js
@@ -13,7 +13,13 @@ class FeedbackWidget {
   }
 
   show (message, type) {
-    console.log('showing %o', this.element)
+    // Normalize
+    type = (type === 'success' ? 'success' : 'error')
+
+    // Log
+    this.log({ message, type })
+
+    // Show
     this.element.text(message)
     this.element.css('display', 'block')
     this.element.toggleClass('alert-success', type === 'success')
@@ -23,6 +29,39 @@ class FeedbackWidget {
   hide () {
     console.log('hideing %s', this.elementId)
     this.element.css('display', 'none')
+  }
+
+  log (message) {
+    // Get log
+    const collection = JSON.parse(localStorage.getItem('feedback_widget')) || []
+
+    // Add item
+    collection.push(message)
+
+    // Remove more than 10 items
+    while (collection.length > 10) {
+      collection.shift()
+    }
+
+    // Write
+    localStorage.setItem('feedback_widget', JSON.stringify(collection))
+  }
+
+  removeLog () {
+    localStorage.removeItem('feedback_widget')
+  }
+
+  history () {
+    // Get log
+    const collection = JSON.parse(localStorage.getItem('feedback_widget')) || []
+
+    // Prep message
+    let logMessage = ''
+
+    // Append lines
+    collection.forEach(({ message, type }) => (logMessage += `${type} - ${message}\n`))
+
+    this.show(logMessage, 'success')
   }
 }
 
