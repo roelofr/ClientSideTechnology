877a69beda79b29204d0801e9d36f51a8c6b88fe
Roelof Roos <github@roelof.io>
2020-02-10 12:22:58 +0100

    Message types

--- js/feedbackWidget.js
+++ js/feedbackWidget.js
@@ -1,19 +1,36 @@
 class FeedbackWidget {
   constructor (elementId) {
     this._elementId = elementId
+    this._element = $(`#${elementId}`)
   }
 
   get elementId () {
     return this._elementId
   }
 
-  show () {
-    console.log('showing %s', this.elementId)
-    document.getElementById(this.elementId).style.display = 'block'
+  get element () {
+    return this._element
+  }
+
+  show (message, type) {
+    console.log('showing %o', this.element)
+    this.element.text(message)
+    this.element.css('display', 'block')
+    this.element.toggleClass('alert-success', type === 'success')
+    this.element.toggleClass('alert-danger', type !== 'success')
   }
 
   hide () {
     console.log('hideing %s', this.elementId)
-    document.getElementById(this.elementId).style.display = 'none'
+    this.element.css('display', 'none')
   }
 }
+
+$(function () {
+  const modal = new FeedbackWidget('feedback')
+  $('[data-action="feedback"]').click(function () {
+    const message = $(this).data('message')
+    const type = $(this).data('type')
+    modal.show(message, type)
+  })
+})
