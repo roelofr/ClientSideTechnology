c02e013687a9f02ca81a677df94ba66fb648a8fa
Roelof Roos <github@roelof.io>
2020-02-12 14:58:50 +0100

    Level 2, ex 3: init config

--- js/game.js
+++ js/game.js
@@ -1,12 +1,22 @@
-const Game = (function () {
+const apiUrl = 'url/super/duper/game'
+
+const Game = (function (url) {
+  const configMap = {
+    api: url
+  }
+
   // Log start
-  console.log('hallo, vanuit een module')
+  console.log('hallo, vanuit een module met config map %o', configMap)
 
   // Private function init
   const init = function () {
-    console.log('Private information!')
+    console.log('Private information, including %s!', configMap.api)
+  }
+
+  const getConfigMap = function () {
+    return configMap
   }
 
   // Waarde/object geretourneerd aan de outer scope
-  return { init }
-})()
+  return { init, getConfigMap }
+})(apiUrl)
