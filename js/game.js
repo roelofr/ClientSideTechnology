2692d030403f43f798936cbf9ddfb504b33ec731
Roelof Roos <github@roelof.io>
2020-02-12 15:03:39 +0100

    Level 2, ex 7

--- js/game.js
+++ js/game.js
@@ -37,3 +37,38 @@ Game.Reversi = (function () {
   // Waarde/object geretourneerd aan de outer scope
   return { init }
 })()
+
+
+// Add 'data' module
+Game.Data = (function () {
+  // unused
+  const configMap = {}
+
+  // Log start
+  console.log('hallo, vanuit data module met config map %o', configMap)
+
+  // Private function init
+  const init = function () {
+    console.log('Private information, including %s!', configMap.api)
+  }
+
+  // Waarde/object geretourneerd aan de outer scope
+  return { init }
+})()
+
+// Add 'model' module
+Game.Model = (function () {
+  // unused
+  const configMap = {}
+
+  // Log start
+  console.log('hallo, vanuit model module met config map %o', configMap)
+
+  // Private function init
+  const init = function () {
+    console.log('Private information, including %s!', configMap.api)
+  }
+
+  // Waarde/object geretourneerd aan de outer scope
+  return { init }
+})()
