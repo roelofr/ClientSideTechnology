ce69639731e7ee4eedc68d1fabce74bf3cfb716a
Roelof Roos <github@roelof.io>
2020-02-12 14:17:18 +0100

    Level 2, ex 2: Init function

--- js/game.js
+++ js/game.js
@@ -2,6 +2,11 @@ const Game = (function () {
   // Log start
   console.log('hallo, vanuit een module')
 
-  // Return nothing yet, but it's defined!
-  return null
+  // Private function init
+  const init = function () {
+    console.log('Private information!')
+  }
+
+  // Waarde/object geretourneerd aan de outer scope
+  return { init }
 })()
