a75c606fbb4930a3fe6bb3ab7a12592a7c23ce60
Roelof Roos <github@roelof.io>
2020-06-26 11:15:31 +0200

    unit tests

---
+++ app/SecurityApp.Tests/LoginTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SecurityApp.Tests
+{
+    [TestClass]
+    public class LoginTest
+    {
+        [TestMethod]
+        public void TestGetLogin()
+        {
+        }
+    }
+}
