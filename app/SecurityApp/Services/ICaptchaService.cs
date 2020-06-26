07ee172d5344d731380282dfdfce73915e87408a
Roelof Roos <github@roelof.io>
2020-06-26 15:49:48 +0200

    Fuck off C#

--- app/SecurityApp/Services/ICaptchaService.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace SecurityApp.Services
-{
-    public interface ICaptchaService {
-        Task<bool> ValidateTokenAsync(string submitToken, string ipAddress);
-        string getPublicToken();
-    }
-}
